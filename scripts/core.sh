eb4aa543359842a2a3da574f5fef23eb04044ed9
RJ <audittnfs@gmail.com>
2017-05-09 15:28:59 +0300

    refactor

--- scripts/core.sh
+++ scripts/core.sh
@@ -12,17 +12,17 @@ core_dir="git"
 create_project_dir()
 {
   echo "Project name: "
-  read project_name
+  read -r project_name
 }
 
 gems_install()
 {
   gems=(compass susy breakpoint)
   for i in ${gems[*]}; do
-    if gem list $i -i; then
+    if gem list "$i" -i; then
       echo gem "$i" already installed
     else
-      gem install $i
+      gem install "$i"
     fi
   done
 }
@@ -31,8 +31,7 @@ success_func()
 {
   echo "+ gulp created"
 
-  npm install
-  if [ $? -eq 0 ]; then
+  if npm install; then
     echo "+ npm install"
   else
     echo "- fail then intall npm"
@@ -42,7 +41,7 @@ success_func()
 create_scaffold() 
 {
   echo "create empty scaffold (y/n)?"
-  read answer
+  read -r answer
   if [ "$answer" = "y" ]; then
     #create files in directory
     mkdir sass js images
@@ -53,12 +52,12 @@ create_scaffold()
 
 core_status="done"
 
-if [ $1 ]; then
+if [ "$1" ]; then
   project_name=$1
 else
   create_project_dir
 fi
-while [ -d $project_name ]
+while [ -d "$project_name" ]
 do
   echo "- this name already exist, try again"
   create_project_dir
@@ -66,15 +65,13 @@ done
 echo "--------------"
 
 #create project directory
-mkdir $project_name
-mkdir $project_name/source
+mkdir "$project_name"
+mkdir "$project_name"/source
 echo "+ project directory created"
-cd $project_name/source
+cd "$project_name"/source
 
 if [ "$core_dir" = "git" ]; then
-  git clone git@gitlab.com:StudiaUtEdamVivo/core.git
-
-  if [ $? -eq 0 ]; then
+  if git clone git@gitlab.com:StudiaUtEdamVivo/core.git; then
     rm core/README.md && rm -rf core/.git
     mv core/* ./
     # mv ./core/{index.html,sass,js} ./
@@ -87,9 +84,7 @@ if [ "$core_dir" = "git" ]; then
   fi
 else
   # cp ${core_dir}index.html ./ && cp -rf ${core_dir}sass/ ./sass/ && cp -rf ${core_dir}js/ ./js/
-  cp -r ${core_dir}* ./
-
-  if [ $? -eq 0 ]; then
+  if cp -r ${core_dir}* ./; then
     rm README.md
     echo "+ html,js,sass,images copied"
   else
@@ -101,10 +96,9 @@ fi
 cd ../
 
 #module bundler
-if [ "$gulp_directory" = "git" -a "$core_status" = "done" ]; then
-  git clone git@gitlab.com:StudiaUtEdamVivo/gulp.git
+if [ "$gulp_directory" = "git" ] && [ "$core_status" = "done" ]; then
 
-  if [ $? -eq 0 ]; then
+  if git clone git@gitlab.com:StudiaUtEdamVivo/gulp.git; then
     mv ./gulp/{config.rb,gulpfile.js,package.json} ./
     rm -rf gulp
     success_func
@@ -113,9 +107,8 @@ if [ "$gulp_directory" = "git" -a "$core_status" = "done" ]; then
     core_status="fail"
   fi
 else
-  cp ${gulp_directory}{gulpfile.js,config.rb,package.json} ./
 
-  if [ $? -eq 0 ]; then
+  if cp ${gulp_directory}{gulpfile.js,config.rb,package.json} ./; then
     success_func
   else
     echo "- fail to clone gulp files"
@@ -135,7 +128,7 @@ if [ "$core_status" = "done" ]; then
   # npm run gulp production
   # echo "+ _production compiled"
   echo "run gulp? (y/n)"
-  read -n 1 answer2
+  read -r -n 1 answer2
   if [ "$answer2" = "y" ]; then
     echo -e "\nok > gulp starting"
     explorer .
